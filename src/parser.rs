6ec39640f5d9c0b741fb1fb5ed7a15f2f22bc39c
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-12-02 20:57:31 +0100

    Simplify code

--- src/parser.rs
+++ src/parser.rs
@@ -22,11 +22,10 @@ pub fn parse_move(input: &str) -> Option<(Move, i32)> {
     let up = map(tag("up"), |_| Move::Up);
     let down = map(tag("down"), |_| Move::Down);
     let fwd = map(tag("forward"), |_| Move::Forward);
-    let spaces = space1;
-    let num = map_res(digit1, |s: &str| s.parse::<i32>());
     let mov = alt((up, down, fwd));
+    let num = map_res(digit1, |s: &str| s.parse::<i32>());
 
-    let res: Result<(&str, (_, _, i32)), E> = tuple((mov, spaces, num))(input);
+    let res: Result<_, E> = tuple((mov, space1, num))(input);
     match res {
         Ok((_, (m, _, n))) => Some((m, n)),
         Err(_) => None,
@@ -42,6 +41,9 @@ mod tests {
         assert_eq!(parse_move("up 4"), Some((Move::Up, 4)));
         assert_eq!(parse_move("down 2"), Some((Move::Down, 2)));
         assert_eq!(parse_move("forward 10"), Some((Move::Forward, 10)));
+        assert_eq!(parse_move("forward -10"), None);
+        assert_eq!(parse_move("up    "), None);
+        assert_eq!(parse_move("5"), None);
         assert_eq!(parse_move("bla"), None);
     }
 }
