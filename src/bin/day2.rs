553c8c070e63c6cbd06a7d27b8c09fd17deae6fd
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-12-02 20:59:15 +0100

    Simplify imports in day2 main

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,11 +1,14 @@
+use aoc2021::files::read_lines;
+use aoc2021::parser::{parse_move, Move};
+
 fn main() {
-    if let Ok(lines) = aoc2021::files::read_lines("./day2/input.txt") {
-        let moves = lines.map(|line| aoc2021::parser::parse_move(&line.unwrap()).unwrap());
+    if let Ok(lines) = read_lines("./day2/input.txt") {
+        let moves = lines.map(|line| parse_move(&line.unwrap()).unwrap());
 
         let apply_move = |(x, y, a), (mv, dy)| match mv {
-            aoc2021::parser::Move::Up => (x, y, a - dy),
-            aoc2021::parser::Move::Down => (x, y, a + dy),
-            aoc2021::parser::Move::Forward => (x + dy, y + (a * dy), a),
+            Move::Up => (x, y, a - dy),
+            Move::Down => (x, y, a + dy),
+            Move::Forward => (x + dy, y + (a * dy), a),
         };
 
         let final_pos = moves.fold((0, 0, 0), apply_move);
