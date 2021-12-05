2c07704603c7ee97c0f70feb2e243e7d5aa0486b
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-12-05 10:32:21 +0100

    Add test for drawing lines

--- src/vents.rs
+++ src/vents.rs
@@ -1,15 +1,26 @@
+use std::cmp::max;
 use std::fs::read_to_string;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Pos {
-    x: u32,
-    y: u32,
+    x: usize,
+    y: usize,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Vent {
     from: Pos,
     to: Pos,
+}
+
+pub fn positions(vent: &Vent) -> Vec<Pos> {
+    let mut res = vec![];
+    for i in vent.from.x..(vent.to.x + 1) {
+        for j in vent.from.y..(vent.to.y + 1) {
+            res.push(Pos { x: i, y: j })
+        }
+    }
+    res.clone()
 }
 
 pub fn is_ortho(vent: &Vent) -> bool {
@@ -20,20 +31,36 @@ fn parse_vents(input: &Vec<&str>) -> Option<Vec<Vent>> {
     None
 }
 
-pub fn draw_lines(board: &mut Vec<Vec<u8>>, vents: Vec<Vent>) {}
+fn draw_line(board: &mut Vec<Vec<u8>>, vent: &Vent) {
+    for pos in positions(vent) {
+        board[pos.y][pos.x] = 1;
+    }
+}
+
+pub fn draw_lines(board: &mut Vec<Vec<u8>>, vents: Vec<Vent>) {
+    for vent in vents {
+        draw_line(board, &vent);
+    }
+}
 
 pub fn intersections(board: &Vec<Vec<u8>>) -> u32 {
     0
 }
 
-fn max_pos(vents: &Vec<Vent>) -> (u32, u32) {
-    vents.iter().fold((0, 0), |(cur_x, cur_y), vent| (0, 0))
+fn bounds(vents: &Vec<Vent>) -> (usize, usize) {
+    let (x, y) = vents.iter().fold((0, 0), |(cur_x, cur_y), vent| {
+        (
+            max(max(cur_x, vent.from.x), vent.to.x),
+            max(max(cur_y, vent.from.y), vent.to.y),
+        )
+    });
+    (x + 1, y + 1)
 }
 
-pub fn parse(file: &str) -> Option<(Vec<Vent>, u32, u32)> {
+pub fn parse(file: &str) -> Option<(Vec<Vent>, usize, usize)> {
     if let Ok(input) = read_to_string(file) {
         if let Some(vents) = parse_vents(&input.split("\n").collect::<Vec<&str>>()) {
-            let (max_x, max_y) = max_pos(&vents);
+            let (max_x, max_y) = bounds(&vents);
             return Some((vents, max_x, max_y));
         }
     }
@@ -96,6 +123,86 @@ mod tests {
                 .collect::<Vec<Vent>>()
                 .len(),
             6
+        );
+    }
+
+    #[test]
+    fn test_retrieve_bounds_from_vents() {
+        let vents = vec![
+            Vent {
+                from: Pos { x: 0, y: 9 },
+                to: Pos { x: 5, y: 9 },
+            },
+            Vent {
+                from: Pos { x: 8, y: 0 },
+                to: Pos { x: 0, y: 8 },
+            },
+            Vent {
+                from: Pos { x: 9, y: 4 },
+                to: Pos { x: 3, y: 4 },
+            },
+            Vent {
+                from: Pos { x: 2, y: 2 },
+                to: Pos { x: 2, y: 1 },
+            },
+            Vent {
+                from: Pos { x: 7, y: 0 },
+                to: Pos { x: 7, y: 4 },
+            },
+            Vent {
+                from: Pos { x: 6, y: 4 },
+                to: Pos { x: 2, y: 0 },
+            },
+            Vent {
+                from: Pos { x: 0, y: 9 },
+                to: Pos { x: 2, y: 9 },
+            },
+            Vent {
+                from: Pos { x: 3, y: 4 },
+                to: Pos { x: 1, y: 4 },
+            },
+            Vent {
+                from: Pos { x: 0, y: 0 },
+                to: Pos { x: 8, y: 8 },
+            },
+            Vent {
+                from: Pos { x: 5, y: 5 },
+                to: Pos { x: 8, y: 2 },
+            },
+        ];
+
+        assert_eq!(bounds(&vents), (10, 10));
+    }
+
+    #[test]
+    fn test_can_draw_a_vertical_line() {
+        let vents = vec![Vent {
+            from: Pos { x: 1, y: 0 },
+            to: Pos { x: 1, y: 3 },
+        }];
+        let mut board = vec![vec![0; 4]; 4];
+
+        draw_lines(&mut board, vents);
+
+        assert_eq!(
+            board,
+            [[0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]]
+        );
+    }
+
+    #[test]
+    fn test_can_draw_a_horizontal_line() {
+        let vents = vec![Vent {
+            from: Pos { x: 0, y: 3 },
+            to: Pos { x: 3, y: 3 },
+        }];
+        let mut board = vec![vec![0; 4]; 4];
+
+        draw_lines(&mut board, vents);
+
+        assert_eq!(
+            board,
+            [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1]]
         );
     }
 }
