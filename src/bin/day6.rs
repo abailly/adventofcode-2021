b7ec35f79e7410b2e548a9d506185f64f72fa85a
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-12-06 08:35:30 +0100

    Completed Day6, part 2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -3,21 +3,27 @@ use aoc2021::parser::parse_file;
 use std::env;
 use std::process;
 
-fn evolve(fishes: &mut Vec<i32>, days: u32) {
+fn evolve(fishes: &mut [u64; 9], days: u32) {
     for _ in 0..days {
-        let mut new_fishes = vec![];
-        for i in 0..fishes.len() {
-            let f = fishes[i];
-            fishes[i] = if f == 0_i32 {
-                new_fishes.push(8);
-                6
-            } else {
-                f - 1_i32
-            }
+        let mut new_fishes = [0_u64; 9];
+        println!("{:?}", fishes);
+        for i in 1..fishes.len() {
+            new_fishes[i - 1] = fishes[i];
         }
-        println!("{:?}", new_fishes);
-        fishes.append(&mut new_fishes);
+        new_fishes[8] = fishes[0];
+        new_fishes[6] += fishes[0];
+        for i in 0..fishes.len() {
+            fishes[i] = new_fishes[i];
+        }
     }
+}
+
+fn fill(fishes: &mut [u64; 9], lanternfishes: Vec<i32>) {
+    for i in 0..fishes.len() {
+        fishes[i] = 0;
+    }
+
+    lanternfishes.iter().for_each(|f| fishes[*f as usize] += 1);
 }
 
 fn main() {
@@ -27,10 +33,12 @@ fn main() {
         process::exit(1);
     }
 
+    let mut fishes = [0_u64; 9];
+
     if let Some(lanternfishes) = parse_file(&args[1], parse_csv) {
-        let mut fishes = lanternfishes;
-        evolve(&mut fishes, 80);
-        println!("{}", fishes.len());
+        fill(&mut fishes, lanternfishes);
+        evolve(&mut fishes, 256);
+        println!("{}", fishes.iter().fold(0, |n, i| n + i));
     } else {
         println!("fail to parse {}", args[1]);
     }
