f07d54736f70d9a52e23eb3b50057a3d4c22becb
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-12-08 18:45:46 +0100

    Extracted encoding function to clarify things

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -8,7 +8,7 @@ struct DigitLine<'a> {
     digits: Vec<&'a str>,
 }
 
-fn is_simple(s: &str) -> bool {
+fn is_simple(s: &&str) -> bool {
     let ln = s.len();
     ln == 2 || ln == 3 || ln == 4 || ln == 7
 }
@@ -18,39 +18,52 @@ fn as_bits(s: &str) -> u8 {
         .fold(0, |n, c| n + (1 << ((c as u8) - ('a' as u8))))
 }
 
+fn encoding_for_690(digits: &[u8; 10], bits: u8) -> usize {
+    if bits & digits[4] == digits[4] {
+        9
+    } else if bits & digits[7] == digits[7] {
+        0
+    } else {
+        6
+    }
+}
+
+fn encoding_for_235(digits: &[u8; 10], bits: u8) -> usize {
+    if bits & digits[1] == digits[1] {
+        3
+    } else if bits & digits[6] == bits {
+        5
+    } else {
+        2
+    }
+}
+
+fn encoding_for_1478(_digits: &[u8; 10], bits: u8) -> usize {
+    match bits.count_ones() {
+        2 => 1,
+        3 => 7,
+        4 => 4,
+        7 => 8,
+        _ => panic!("unknown encoding"),
+    }
+}
+
 fn encode(codes: &Vec<&str>) -> [u8; 10] {
     let mut digits: [u8; 10] = [0; 10];
-    // first handle simple digits
-    codes.iter().filter(|s| is_simple(s)).for_each(|s| {
-        let bits = as_bits(s);
-        match s.len() {
-            2 => digits[1] = bits,
-            3 => digits[7] = bits,
-            4 => digits[4] = bits,
-            7 => digits[8] = bits,
-            _ => panic!("unknown encoding"),
-        }
-    });
-    codes.iter().filter(|s| s.len() == 6).for_each(|s| {
-        let bits = as_bits(s);
-        if bits & digits[4] == digits[4] {
-            digits[9] = bits;
-        } else if bits & digits[7] == digits[7] {
-            digits[0] = bits;
-        } else {
-            digits[6] = bits;
-        }
-    });
-    codes.iter().filter(|s| s.len() == 5).for_each(|s| {
-        let bits = as_bits(s);
-        if bits & digits[1] == digits[1] {
-            digits[3] = bits;
-        } else if bits & digits[6] == bits {
-            digits[5] = bits;
-        } else {
-            digits[2] = bits;
-        }
-    });
+
+    let mut encoder = |filter: for<'a> fn(&'a &&str) -> bool,
+                       encoding: fn(&[u8; 10], u8) -> usize| {
+        codes.iter().filter(filter).for_each(|s| {
+            let bits = as_bits(s);
+            let pos = encoding(&digits, bits);
+            digits[pos] = bits;
+        })
+    };
+
+    encoder(|s| is_simple(s), encoding_for_1478);
+    encoder(|s| s.len() == 6, encoding_for_690);
+    encoder(|s| s.len() == 5, encoding_for_235);
+
     digits
 }
 
