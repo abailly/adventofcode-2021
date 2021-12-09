c10aa541806242c9dce268fbdee4e8f04bcd0565
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-12-09 18:15:45 +0100

    Completed Day 2, part 2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -16,19 +16,85 @@ fn is_low_point(x: &u8, a: &u8, b: &u8, c: &u8, d: &u8) -> bool {
     x < a && x < b && x < c && x < d
 }
 
-fn solve(nums: &Vec<(u8, u8, u8, u8, u8)>) -> u64 {
-    let mut sum = 0_u64;
-    for (x, a, b, c, d) in nums {
-        if is_low_point(x, a, b, c, d) {
-            sum += 1_u64 + *x as u64;
-        }
+fn neighbours(nums: &Vec<Vec<(u8, u8, u8, u8, u8)>>, pos: (usize, usize)) -> Vec<(usize, usize)> {
+    let mut res = vec![];
+    let (i, j) = pos;
+    if i > 0 {
+        res.push((i - 1, j));
     }
-    sum.into()
+    if i < nums[0].len() - 1 {
+        res.push((i + 1, j));
+    }
+    if j > 0 {
+        res.push((i, (j - 1)));
+    }
+    if j < nums.len() - 1 {
+        res.push((i, (j + 1)));
+    }
+    res
 }
 
-fn transform(input: Vec<Vec<u8>>) -> Vec<(u8, u8, u8, u8, u8)> {
+fn find_basins(
+    nums: &Vec<Vec<(u8, u8, u8, u8, u8)>>,
+    low_points: &Vec<(usize, usize)>,
+) -> Vec<u64> {
+    let mut res = vec![];
+    println!("low {:?}", low_points);
+    for (i, j) in low_points {
+        let mut basin = vec![];
+        let mut visited = vec![];
+        let mut cur_neighbours = vec![(*i, *j)];
+        println!("cur low {:?} neighbours {:?}", (i, j), cur_neighbours);
+        while !cur_neighbours.is_empty() {
+            if let Some(cur) = cur_neighbours.pop() {
+                let cur_neigh = neighbours(nums, cur);
+                println!("cur {:?}, neigbours {:?}", cur, cur_neigh);
+                for (x, y) in cur_neigh {
+                    if nums[y][x].0 != 9
+                        && !visited.contains(&(x, y))
+                        && !basin.contains(&(x, y))
+                        && !cur_neighbours.contains(&(x, y))
+                    {
+                        println!("adding {:?}", (x, y));
+                        cur_neighbours.push((x, y));
+                    }
+                }
+                if nums[cur.1][cur.0].0 != 9 {
+                    basin.push(cur);
+                }
+                visited.push(cur);
+            }
+        }
+        res.push(basin.len() as u64);
+        println!("pushing {:?} into res {:?}", basin, res);
+    }
+
+    res
+}
+
+fn find_low_points(nums: &Vec<Vec<(u8, u8, u8, u8, u8)>>) -> Vec<(usize, usize)> {
+    let mut res = vec![];
+    for (j, row) in nums.iter().enumerate() {
+        for (i, (x, a, b, c, d)) in row.iter().enumerate() {
+            if is_low_point(x, a, b, c, d) {
+                res.push((i, j));
+            }
+        }
+    }
+    res
+}
+
+fn solve(nums: &Vec<Vec<(u8, u8, u8, u8, u8)>>) -> u64 {
+    let mut res = find_basins(&nums, &find_low_points(&nums));
+    res.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    println!("{:?}", res);
+    res[0..3].iter().fold(1, |n, b| n * b)
+}
+
+fn transform(input: Vec<Vec<u8>>) -> Vec<Vec<(u8, u8, u8, u8, u8)>> {
     let mut output = vec![];
     for (j, row) in input.iter().enumerate() {
+        let mut new_row = vec![];
         for (i, v) in row.iter().enumerate() {
             let mut t = (*v, 10, 10, 10, 10);
             if i > 0 {
@@ -43,8 +109,9 @@ fn transform(input: Vec<Vec<u8>>) -> Vec<(u8, u8, u8, u8, u8)> {
             if j < input.len() - 1 {
                 t.4 = input[j + 1][i];
             }
-            output.push(t);
+            new_row.push(t);
         }
+        output.push(new_row);
     }
     output
 }
@@ -71,7 +138,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn can_find_low_points() {
+    fn can_solve_part_1() {
         let sample = vec![
             vec![2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
             vec![3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
@@ -83,5 +150,35 @@ mod tests {
         let res = solve(&transform(sample));
 
         assert_eq!(res, 15);
+    }
+
+    #[test]
+    fn can_find_low_points() {
+        let sample = vec![
+            vec![2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+            vec![3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+            vec![9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+            vec![8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+            vec![9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
+        ];
+
+        let res = find_low_points(&transform(sample));
+
+        assert_eq!(res, vec![(1, 0), (9, 0), (2, 2), (6, 4)]);
+    }
+
+    #[test]
+    fn can_find_basins() {
+        let sample = transform(vec![
+            vec![2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+            vec![3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+            vec![9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+            vec![8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+            vec![9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
+        ]);
+
+        let res = find_basins(&sample, &find_low_points(&sample));
+
+        assert_eq!(res, vec![3, 9, 14, 9]);
     }
 }
