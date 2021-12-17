23467c01ae2139682f9ee67d7c44c053e300863f
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-12-17 17:25:47 +0100

    Can parse type0 operators but not type 1

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -8,6 +8,7 @@ use nom::multi::many0;
 use nom::sequence::tuple;
 use nom::IResult;
 use nom::Parser;
+use std::convert::TryInto;
 use std::env;
 use std::fs::read_to_string;
 use std::process;
@@ -61,65 +62,71 @@ fn parse_value(input: &str) -> IResult<&str, Packet> {
     value(input)
 }
 
-// fn parse_operator_0(input: (&[u8], usize)) -> IResult<(&[u8], usize), Packet> {
-//     let mut op_prefix = tuple((take(3usize), take(3usize), tag(0x0, 1usize), take(15usize)));
-//     match op_prefix(input) {
-//         Ok(((bytes, off), (version, t, _, len))) => {
-//             let remaining = bytes.len() * 8 - off;
-//             let mut consumed = 0;
-//             let mut pkts = vec![];
-//             let mut inp = (bytes, off);
-//             while consumed < len {
-//                 match parse_packet(inp) {
-//                     Ok(((nbytes, noff), p)) => {
-//                         pkts.push(p);
-//                         consumed = consumed + (remaining - (nbytes.len() * 8 - noff));
-//                         inp = (nbytes, noff);
-//                     }
-//                     Err(e) => return Err(e),
-//                 }
-//             }
-//             Ok((
-//                 inp,
-//                 Packet {
-//                     version,
-//                     content: Content::Operator(t, pkts),
-//                 },
-//             ))
-//         }
-//         Err(e) => Err(e),
-//     }
-// }
+fn op_prefix<'a>(s: &str, input: &'a str) -> IResult<&'a str, (u8, u8, (), u64)> {
+    tuple((
+        take(3usize).map(|n| as_num(n) as u8),
+        take(3usize).map(|n| as_num(n) as u8),
+        tag(s).map(|_| ()),
+        take(15usize).map(|n| as_num(n)),
+    ))(input)
+}
 
-// fn parse_operator_1(input: &str) -> IResult<&str, Packet> {
-//     let mut op_prefix = tuple((take(3usize), take(3usize), tag(0x01, 1usize), take(11usize)));
-//     match op_prefix(input) {
-//         Ok((inp, (version, t, _, len))) => {
-//             let mut remaining: usize = len;
-//             let mut pkts = vec![];
-//             let mut stream = inp;
-//             while remaining > 0 {
-//                 match parse_packet(stream) {
-//                     Ok((ninp, p)) => {
-//                         pkts.push(p);
-//                         stream = ninp;
-//                         remaining -= 1;
-//                     }
-//                     Err(e) => return Err(e),
-//                 }
-//             }
+fn parse_operator_0(input: &str) -> IResult<&str, Packet> {
+    match op_prefix("0", input) {
+        Ok((bs, (version, t, _, len))) => {
+            let mut consumed = 0usize;
+            let mut pkts = vec![];
+            let mut inp = bs;
+            while consumed < len.try_into().unwrap() {
+                match parse_packet(inp) {
+                    Ok((more, p)) => {
+                        pkts.push(p);
+                        consumed = consumed + (bs.len() - more.len());
+                        inp = more;
+                    }
+                    Err(e) => return Err(e),
+                }
+            }
+            Ok((
+                inp,
+                Packet {
+                    version,
+                    content: Content::Operator(t, pkts),
+                },
+            ))
+        }
+        Err(e) => Err(e),
+    }
+}
 
-//             Ok((
-//                 stream,
-//                 Packet {
-//                     version,
-//                     content: Content::Operator(t, pkts),
-//                 },
-//             ))
-//         }
-//         Err(e) => Err(e),
-//     }
-// }
+fn parse_operator_1(input: &str) -> IResult<&str, Packet> {
+    match op_prefix("1", input) {
+        Ok((inp, (version, t, _, len))) => {
+            let mut remaining: usize = len as usize;
+            let mut pkts = vec![];
+            let mut stream = inp;
+            while remaining > 0 {
+                match parse_packet(stream) {
+                    Ok((ninp, p)) => {
+                        pkts.push(p);
+                        stream = ninp;
+                        remaining -= 1;
+                    }
+                    Err(e) => return Err(e),
+                }
+            }
+
+            Ok((
+                stream,
+                Packet {
+                    version,
+                    content: Content::Operator(t, pkts),
+                },
+            ))
+        }
+        Err(e) => Err(e),
+    }
+}
 
 pub fn to_bits(s: &str) -> String {
     let mut res = String::new();
@@ -150,7 +157,7 @@ pub fn to_bits(s: &str) -> String {
 
 fn parse_packet(input: &str) -> IResult<&str, Packet> {
     println!("parsing {:?}", input);
-    parse_value(input)
+    alt((parse_value, parse_operator_0, parse_operator_1))(input)
 }
 
 fn parse_packets(input: &str) -> Option<Packet> {
@@ -207,120 +214,121 @@ mod tests {
             })
         );
     }
-    // #[test]
-    // fn can_parse_operator_packet_with_len_0() {
-    //     let input = "38006F45291200";
 
-    //     let res = parse_packets(&input);
+    #[test]
+    fn can_parse_operator_packet_with_len_0() {
+        let input = "38006F45291200";
 
-    //     assert_eq!(
-    //         res,
-    //         Some(Packet {
-    //             version: 1,
-    //             content: Content::Operator(
-    //                 6,
-    //                 vec![
-    //                     Packet {
-    //                         version: 6,
-    //                         content: Content::Value(10)
-    //                     },
-    //                     Packet {
-    //                         version: 2,
-    //                         content: Content::Value(20)
-    //                     }
-    //                 ]
-    //             )
-    //         })
-    //     );
-    // }
+        let res = parse_packets(&input);
 
-    // #[test]
-    // fn can_parse_operator_packet_with_len_1() {
-    //     let input = "EE00D40C823060";
+        assert_eq!(
+            res,
+            Some(Packet {
+                version: 1,
+                content: Content::Operator(
+                    6,
+                    vec![
+                        Packet {
+                            version: 6,
+                            content: Content::Value(10)
+                        },
+                        Packet {
+                            version: 2,
+                            content: Content::Value(20)
+                        }
+                    ]
+                )
+            })
+        );
+    }
 
-    //     let res = parse_packets(&input);
+    #[test]
+    fn can_parse_operator_packet_with_len_1() {
+        let input = "EE00D40C823060";
 
-    //     assert_eq!(
-    //         res,
-    //         Some(Packet {
-    //             version: 7,
-    //             content: Content::Operator(
-    //                 3,
-    //                 vec![
-    //                     Packet {
-    //                         version: 2,
-    //                         content: Content::Value(1)
-    //                     },
-    //                     Packet {
-    //                         version: 4,
-    //                         content: Content::Value(2)
-    //                     },
-    //                     Packet {
-    //                         version: 1,
-    //                         content: Content::Value(3)
-    //                     }
-    //                 ]
-    //             )
-    //         })
-    //     );
-    // }
+        let res = parse_packets(&input);
 
-    // #[test]
-    // fn can_compute_total_version() {
-    //     let other = "620080001611562C8802118E34";
-    //     let input = "A0016C880162017C3686B18A3D4780";
+        assert_eq!(
+            res,
+            Some(Packet {
+                version: 7,
+                content: Content::Operator(
+                    3,
+                    vec![
+                        Packet {
+                            version: 2,
+                            content: Content::Value(1)
+                        },
+                        Packet {
+                            version: 4,
+                            content: Content::Value(2)
+                        },
+                        Packet {
+                            version: 1,
+                            content: Content::Value(3)
+                        }
+                    ]
+                )
+            })
+        );
+    }
 
-    //     let pkt = parse_packets(&input).unwrap();
+    #[test]
+    fn can_compute_total_version() {
+        let other = "620080001611562C8802118E34";
+        let input = "A0016C880162017C3686B18A3D4780";
 
-    //     assert_eq!(versions(&parse_packets(&other).unwrap()), 12);
-    //     assert_eq!(
-    //         versions(&parse_packets("C0015000016115A2E0802F182340").unwrap()),
-    //         23
-    //     );
-    //     assert_eq!(versions(&parse_packets("8A004A801A8002F478").unwrap()), 16);
-    //     assert_eq!(versions(&pkt), 31);
-    //     assert_eq!(
-    //         pkt,
-    //         Packet {
-    //             version: 5,
-    //             content: Content::Operator(
-    //                 0,
-    //                 vec![Packet {
-    //                     version: 1,
-    //                     content: Content::Operator(
-    //                         0,
-    //                         vec![Packet {
-    //                             version: 3,
-    //                             content: Content::Operator(
-    //                                 0,
-    //                                 vec![
-    //                                     Packet {
-    //                                         version: 7,
-    //                                         content: Content::Value(6)
-    //                                     },
-    //                                     Packet {
-    //                                         version: 6,
-    //                                         content: Content::Value(6)
-    //                                     },
-    //                                     Packet {
-    //                                         version: 5,
-    //                                         content: Content::Value(12)
-    //                                     },
-    //                                     Packet {
-    //                                         version: 2,
-    //                                         content: Content::Value(15)
-    //                                     },
-    //                                     Packet {
-    //                                         version: 2,
-    //                                         content: Content::Value(15)
-    //                                     }
-    //                                 ]
-    //                             )
-    //                         }]
-    //                     )
-    //                 }]
-    //             )
-    //         }
-    //     );
-    // }
+        let pkt = parse_packets(&input).unwrap();
+
+        assert_eq!(versions(&parse_packets(&other).unwrap()), 12);
+        assert_eq!(
+            versions(&parse_packets("C0015000016115A2E0802F182340").unwrap()),
+            23
+        );
+        assert_eq!(versions(&parse_packets("8A004A801A8002F478").unwrap()), 16);
+        assert_eq!(versions(&pkt), 31);
+        assert_eq!(
+            pkt,
+            Packet {
+                version: 5,
+                content: Content::Operator(
+                    0,
+                    vec![Packet {
+                        version: 1,
+                        content: Content::Operator(
+                            0,
+                            vec![Packet {
+                                version: 3,
+                                content: Content::Operator(
+                                    0,
+                                    vec![
+                                        Packet {
+                                            version: 7,
+                                            content: Content::Value(6)
+                                        },
+                                        Packet {
+                                            version: 6,
+                                            content: Content::Value(6)
+                                        },
+                                        Packet {
+                                            version: 5,
+                                            content: Content::Value(12)
+                                        },
+                                        Packet {
+                                            version: 2,
+                                            content: Content::Value(15)
+                                        },
+                                        Packet {
+                                            version: 2,
+                                            content: Content::Value(15)
+                                        }
+                                    ]
+                                )
+                            }]
+                        )
+                    }]
+                )
+            }
+        );
+    }
 }
